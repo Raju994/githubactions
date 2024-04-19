2904ce41147a3dffd798e43b0c266d03691999b5
Uppala Raju <vraju9949@gmail.com>
2024-04-19 13:16:48 +0200

    Third commit

--- cypress/e2e/First.spec.cy.js
+++ cypress/e2e/First.spec.cy.js
@@ -4,21 +4,8 @@ describe("This is my first run",()=>{
     
     it("Open CDK Global",()=>{
 
-        cy.visit("https://www.cdkglobal.com/login");
-
-       cy.url().should('eq','https://www.cdkglobal.com/login')
-      
-       cy.get('#_evidon-decline-button').click()
-       cy.wait(1000)
-       cy.get('.hs-collapse-toggle').click()
-       
-       cy.wait(1000)
-       cy.get('.block.font-semi').click()
-       
-       
-    
-       })
-       
+        cypress.log("HELLO!!!!!!")
+    })
 
     })
 
